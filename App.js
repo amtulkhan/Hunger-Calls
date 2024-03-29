4931f613867d02d9b31cf9429dd80b8068d21ce0
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-03-29 19:54:30 +0530

    first commit:first episode

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent", class: "heading" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "H1 Tag"),
+    React.createElement("h2", {}, "H1 Tag"),
+  ]),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h1", {}, "H1 Tag")
+  ),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
