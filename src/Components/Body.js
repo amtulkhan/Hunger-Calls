0392c48aa390087c26539a0777d058e9f698f0b5
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-05-08 23:45:37 +0530

    search functionality added

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,10 @@
 import RestaurantCards from "./RestaurantCard";
-import restaurants from "../Utils/mockData";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [resList, setResList] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fectData();
@@ -15,9 +15,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(
-      json?.data?.cards[1]?.card.card.gridElements.infoWithStyle.restaurants
-    );
+
     setResList(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
@@ -28,17 +26,38 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
-        <button
-          onClick={() => {
-            console.log("click");
-            const filteredList = resList.filter(
-              (res) => res.info.avgRating > 4
-            );
-            setResList(filteredList);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+        <div className="search">
+          <input
+            type="text"
+            className="search-input"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredList = resList.filter((res) =>
+                res.info.name.includes(search)
+              );
+              setResList(filteredList);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              console.log("click");
+              const filteredList = resList.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setResList(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
       <div className="res-container">
         {resList.map((restaurant) => {
