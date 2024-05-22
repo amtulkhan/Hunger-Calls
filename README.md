8d37c53fdf62dc4145ce466374553a7e435e1932
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-05-22 14:40:42 +0530

    readme file updated with bundling learnings

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # React_Practice_01
+
+-- React Class Lifecycle Method
+Construstor
+Render
+componentdidmount
+componentDidupdate
+componentWillUnmount
+
+      -- React functional Lifecycle Method/API
+            UseEffect
+             - takes a callback method and a dependency array(optional)
+             - with just the callback function useeffect is called in every render (MOUNT)
+             - with an empty dependency array useEffect is called in the initial render (MOUNT)
+             - with a dependency useeffect is called only when the dependency array elements updates (UPDATE)
+             - we can return from useEffect and clean up the code (UNMOUNT)
+
+How to optimise our app.
+` treating it as a bundle of many small applications, we can reduce the load of delivering a large application
+it is called in many name -
+-- Bundling
+-- Code splitting
+-- lazy loading
+-- Chunking
+-- On demand loading
--- dist/index.7271efb6.js
+++ dist/index.7271efb6.js
@@ -34748,21 +34748,7 @@ class UserClass extends (0, _reactDefault.default).Component {
             count: 0
         };
     }
-    /*  -- React Class Lifecycle Method 
-            Construstor
-            Render
-            componentdidmount
-            componentDidupdate
-            componentWillUnmount
-        
-      -- React functional Lifecycle Method/API
-            UseEffect
-             - takes a callback method and a dependency array(optional) 
-             - with just the callback function useeffect is called in every render (MOUNT)
-             - with an empty dependency array useEffect is called in the initial render (MOUNT)
-             - with a dependency useeffect is called only when the dependency array elements updates (UPDATE)   
-             - we can return from useEffect and clean up the code (UNMOUNT)  
-*/ render() {
+    render() {
         const { name, location } = this.props;
         const { count } = this.state;
         return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -34772,14 +34758,14 @@ class UserClass extends (0, _reactDefault.default).Component {
                     children: name
                 }, void 0, false, {
                     fileName: "src/Components/UserClass.js",
-                    lineNumber: 31,
+                    lineNumber: 16,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h2", {
                     children: location
                 }, void 0, false, {
                     fileName: "src/Components/UserClass.js",
-                    lineNumber: 32,
+                    lineNumber: 17,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h2", {
@@ -34789,7 +34775,7 @@ class UserClass extends (0, _reactDefault.default).Component {
                     ]
                 }, void 0, true, {
                     fileName: "src/Components/UserClass.js",
-                    lineNumber: 33,
+                    lineNumber: 18,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
@@ -34801,20 +34787,20 @@ class UserClass extends (0, _reactDefault.default).Component {
                     children: "Increase"
                 }, void 0, false, {
                     fileName: "src/Components/UserClass.js",
-                    lineNumber: 34,
+                    lineNumber: 19,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h2", {
                     children: "Software Engineer"
                 }, void 0, false, {
                     fileName: "src/Components/UserClass.js",
-                    lineNumber: 41,
+                    lineNumber: 26,
                     columnNumber: 9
                 }, this)
             ]
         }, void 0, true, {
             fileName: "src/Components/UserClass.js",
-            lineNumber: 30,
+            lineNumber: 15,
             columnNumber: 7
         }, this);
     }
