ff221489ce2d90d212d93ec0ab8292d68c508d8f
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-06-04 14:05:22 +0530

    Ways of writting tests

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -2,8 +2,17 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+describe("Contact Us page test cases", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  //Test can also be written as it
+  it("Should load one heading", () => {
+    render(<Contact />);
+    const heading = screen.getAllByRole("heading");
+    expect(heading.length).toBe(1);
+  });
 });
